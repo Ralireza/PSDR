083f558e3b67c335363fa88419904f075d6394ec
Ralireza <alirezarezaie0101@gmail.com>
2019-06-30 19:50:08 +0430

    add os directory

--- build_dataset/record_data.py
+++ build_dataset/record_data.py
@@ -1,17 +1,18 @@
 import sounddevice as sd
 from scipy.io.wavfile import write
-
+import os
 
 def record_voice():
     fs = 8000
     seconds = 2
     name = input("Please Enter Your Name:")
     label= input("Please Enter Your Label:")
+    os.mkdir(name)
     for i in range(10):
-        input("Please press key to record voice {}_{}_{}:".format(label,name, i))
+        input("Please press Enter to record voice {}_{}_{}:".format(label,name, i))
         myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
         sd.wait()
-        write('{}_{}_{}.wav'.format(label,name,i), fs, myrecording)
+        write('{}/{}_{}_{}.wav'.format(name,label,name,i), fs, myrecording)
 
 
 record_voice()
