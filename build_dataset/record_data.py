d887f2c7ec5f4ee9bfed93a2ebaf5e9dfb6b0808
alireza <alireza@alirezas-MacBook-Air.local>
2019-06-30 19:39:21 +0430

    add recording script to project

---
+++ build_dataset/record_data.py
@@ -0,0 +1,19 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+
+def record_voice():
+    fs = 8000
+    seconds = 2
+    name = input("Please Enter Your Name:")
+    label= input("Please Enter Your Label:")
+    for i in range(5):
+        input("Please press key to record voice {}_{}_{}:".format(label,name, i))
+        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        sd.wait()
+        write('{}_{}_{}.wav'.format(label,name,i), fs, myrecording)
+
+
+record_voice()
+
+
