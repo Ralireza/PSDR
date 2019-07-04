80024b8e1267894755dfa25ea0e65d5ff84aa748
Ralireza <alirezarezaie0101@gmail.com>
2019-07-04 12:16:05 +0430

    fix some bug in record dataset

--- record_dataset/record_data.py
+++ record_dataset/record_data.py
@@ -3,16 +3,22 @@ from scipy.io.wavfile import write
 import os
 
 def record_voice():
+    # create folders
+    for i in range(10):
+        if not os.path.exists(str(i)):
+            os.mkdir(str(i))
+
     fs = 8000
     seconds = 2
+
     name = input("Please Enter Your Name:")
     label= input("Please Enter Your Label:")
-    os.mkdir(name)
+
     for i in range(10):
         input("Please press Enter to record voice {}_{}_{}:".format(label,name, i))
-        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        my_recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
         sd.wait()
-        write('{}/{}_{}_{}.wav'.format(name,label,name,i), fs, myrecording)
+        write('{}/{}_{}_{}.wav'.format(label,label,name,i), fs, my_recording)
 
 
 record_voice()
