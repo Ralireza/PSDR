a2bc8244993bdc9d78730fda8fb56346d64007df
Ali Zonnour <zonour97@gmail.com>
2019-07-04 18:29:11 +0430

    Ali.Z added dataset

--- dataset/record_data.py
+++ dataset/record_data.py
@@ -16,7 +16,7 @@ def record_voice():
 
     for i in range(10):
         input("Please press Enter to record voice {}_{}_{}:".format(label,name, i))
-        my_recording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+        my_recording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
         sd.wait()
         write('{}/{}_{}_{}.wav'.format(label,label,name,i), fs, my_recording)
 
